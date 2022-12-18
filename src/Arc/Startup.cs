b49e8a47c942250437164c8cb79352b7d84097a0
Note <120763388+ForgottenNote@users.noreply.github.com>
2022-12-17 21:38:50 -0500

    Add files via upload

---
+++ src/Arc/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Arc.Driver.Linux;
+using Arc.Game.Apex;
+using Arc.Logging;
+
+namespace Arc
+{
+    public static class Startup
+    {
+        #region Statics
+
+        public static void ConfigureLogging(ILoggingBuilder builder)
+        {
+            builder.ClearProviders();
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, LoggerProvider>());
+        }
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<Linux>();
+            Bootstrap.ConfigureServices(services);
+        }
+
+        #endregion
+    }
+}
